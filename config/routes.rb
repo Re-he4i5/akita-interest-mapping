460e7a8af2d6bd6e5cb18ec2d065860814a9b21a
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-08-30 21:41:39 +0900

    deviseの導入完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_for :users, controllers: {
+  #   registrations: "publics/registrations",
+  #   sessions: 'publics/sessions'
+  # }
+  devise_for :admins
+  # devise_for :admins, controllers: {
+  #   sessions: 'admins/sessions'
+  # }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
