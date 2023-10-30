b769894c8898dac0eb5b5bffcfd51ffa1fb5536c
recodeyo1ko <kuronekoryohei@gmail.com>
2023-10-31 01:27:42 +0900

    レイアウト調整

--- app/controllers/publics/posts_controller.rb
+++ app/controllers/publics/posts_controller.rb
@@ -35,9 +35,28 @@ class Publics::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @town = Town.find(@post.town_id)
+    @towns = Town.all
+    @categories = Category.all
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      if Town.find_by(name: @post.town_name)
+        town_exist = Town.find_by(name: @post.town_name)
+        @post.update(town_id: town_exist.id)
+      else
+        @post.update(town_id: nil)
+      end
+      redirect_to user_path(current_user.id)
+    else
+      @town = Town.find(@post.town_id)
+      @towns = Town.all
+      @categories = Category.all
+      render :edit
+    end
   end
 
   def destroy
